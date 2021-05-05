8eb4fb478599d09af342bd866efb6171dc2ec0ef
henk687 <henkleenheer@upcmail.nl>
2021-05-05 20:48:42 +0200

    changes

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const cover = document.querySelector('#cover')
 
 // Song titles
 const songs = ['Dr No', 'From Russia with love', 'Goldfinger', 'Thunderball', 'You only live twice', 'On her majesty secrect service', 'Diamonds are forever', 'Live and let die', 
-'The man with the golden gun', 'Moonraker', 'For your eyes only', 'Octopussy', 'A view to a kill', 'The living daylights', 'License to kill', 'Golden eye', 'Tomorrow never dies']
+'The man with the golden gun', 'The spy who loved me', 'Moonraker', 'For your eyes only', 'Octopussy', 'A view to a kill', 'The living daylights', 'License to kill', 'Golden eye', 'Tomorrow never dies']
 
 // Keep track of songs
 let songIndex = 0
